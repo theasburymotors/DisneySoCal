59e53c52eaa529e9583a8308951c3be7c953ff9b
Neal Simpson <nealsimpson@Alexanders-MacBook-Air.local>
2016-03-03 15:07:14 -0500

    Initial commit

---
+++ Shared/build/generated/source/buildConfig/release/com/asburymotors/android/disneysocal/common/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.asburymotors.android.disneysocal.common;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.asburymotors.android.disneysocal.common";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
